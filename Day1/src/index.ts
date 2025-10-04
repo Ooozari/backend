6c469863e30192a13cd399f01eb4a37d74d420ab
Ooozari <uzairasiff1227@gmail.com>
2025-10-04 23:44:40 +0500

    chore: add type definitions for Express and Node.js

--- Day1/src/index.ts
+++ Day1/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Node js Series")
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+// Basic GET route
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
