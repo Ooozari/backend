3bb1d383681c363f4759b069bce67c64ad8a2108
Ooozari <uzairasiff1227@gmail.com>
2025-10-07 21:55:33 +0500

    add field definitions to medical_Record schemas

--- Day3/hospital_management/medical_record.models.js
+++ Day3/hospital_management/medical_record.models.js
@@ -1,8 +1,42 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const medicalRecordSchema = new mongoose.Schema({}, { timestamps: true });
+const medicalRecordSchema = new mongoose.Schema(
+  {
+    patient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Patient",
+      required: true,
+    },
+    doctor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Doctor",
+      required: true,
+    },
+    hospital: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hospital",
+      required: true,
+    },
+    prescribedMedication: {
+      type: String,
+      required: true,
+    },
+    treatmentNotes: {
+      type: String,
+      required: true,
+    },
+    visitedDate: {
+      type: Date, // 5AM 30 sep, 2020
+      required: true,
+    },
+    followUpDate: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
 
 export const MedicalRecord = mongoose.model(
-  'MedicalRecord',
+  "MedicalRecord",
   medicalRecordSchema
 );
