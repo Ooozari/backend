c99daacdbec7dfe47d634954e607705a9c890e8f
Ooozari <uzairasiff1227@gmail.com>
2025-10-19 22:31:19 +0500

    feat(models): add Video schema with user reference and default fields

---
+++ Day4/src/models/video.model.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new Schema({
+    videoFile :{
+        type: String,
+        required: true
+    },
+    thumbnail :{
+        type: String,
+        required: true
+    },
+    owner :{
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    },
+    title :{
+        type: String,
+        required: true,
+        maxLength: [150, "Title cannot exceed 150 characters"]
+    },
+    description :{
+        type: String,
+        required: true,
+        maxLength: [500, "Description cannot exceed 500 characters"]
+    },
+    duration :{
+        type: Number,
+    },
+    views :{
+        type: Number,
+        default: 0
+    },
+    isPublished :{
+        type: Boolean,
+        default: true,
+    },
+}, { timestamps: true });
+
+
+export const Video = mongoose.model("Video", videoSchema)
