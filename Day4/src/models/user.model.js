2164866e437461ef93bd2c30d4676f4a08635be8
Ooozari <uzairasiff1227@gmail.com>
2025-10-23 21:16:02 +0500

    feat(models): add password encryption and validation methods in User schema

--- Day4/src/models/user.model.js
+++ Day4/src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose,{Schema} from 'mongoose'
-
+import bcrypt from 'bcrypt'
 
 const userSchema = new Schema(
     {
@@ -49,5 +49,17 @@ const userSchema = new Schema(
         timestamps: true
     }
 )
+
+// function to encrypt user password
+userSchema.pre('save', async function(){
+    if(!this.password.isModified("password")) return;
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+// function to validate the password
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
 
 export const User = mongoose.model("User", userSchema)
