87a586d826891b73b7a5f910aa1c071671777093
Ooozari <uzairasiff1227@gmail.com>
2025-10-26 14:31:01 +0500

    feat(routes): add user registration test route and integrate with Express app

--- Day4/src/app.js
+++ Day4/src/app.js
@@ -15,8 +15,16 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended:true, limit: "16kb"}))
 app.use(express.static("public"))
 
-// use sceure cookies in user browser
+// use secure cookies in user browser
 app.use(cookieParser())
+
+
+import userRouter from './routes/user.route.js'
+
+app.use("/api/v1/user", userRouter)
+// http://localhost:8000/api/v1/users/register
+
+
 
 export {app}
 
