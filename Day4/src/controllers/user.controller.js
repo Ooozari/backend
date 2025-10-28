10748321a782a6065577c6fe9f800aaadaefd3fa
Ooozari <uzairasiff1227@gmail.com>
2025-10-28 17:22:39 +0500

    fix: added await before the DB call and added an guard in logoutUser

--- Day4/src/controllers/user.controller.js
+++ Day4/src/controllers/user.controller.js
@@ -167,7 +167,11 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  User.findByIdAndUpdate(
+  if (!req.user?._id) {
+    throw new ApiError(401, "Unauthorized request");
+  }
+
+  await User.findByIdAndUpdate(
     req.user._id,
     {
       $set: {
@@ -181,14 +185,12 @@ const logoutUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
-  }
-  return res.
-  status(200)
-  .clearCookie("accessToken", options)
-  .clearCookie("refreshToken", options)
-  .json(
-    new ApiResponse(200, {}, "User logout successful")
-  )
+  };
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logout successful"));
 });
 
 export { registerUser, loginUser, logoutUser };
