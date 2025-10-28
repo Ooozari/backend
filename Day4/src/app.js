1b1ffb22bcef932d1e19fe28b10fd89298073196
Ooozari <uzairasiff1227@gmail.com>
2025-10-28 15:21:18 +0500

    improve the comments

--- Day4/src/app.js
+++ Day4/src/app.js
@@ -22,7 +22,7 @@ app.use(cookieParser())
 import userRouter from './routes/user.route.js'
 
 app.use("/api/v1/user", userRouter)
-// http://localhost:8000/api/v1/users/register
+// http://localhost:8000/api/v1/user/register
 
 
 
